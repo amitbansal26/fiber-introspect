ae8295e1e2b66bf014983dc95e6e56547c4327aa
arsmn <arsmn1996@gmail.com>
2020-06-05 02:12:59 +0430

    Update dependencies

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/arsmn/fiber-introspect
 
 go 1.11
 
-require github.com/gofiber/fiber v1.9.3
+require (
+	github.com/arsmn/oauth2-introspection v0.0.1
+	github.com/gofiber/fiber v1.10.5
+)
--- introspect.go
+++ introspect.go
@@ -1,25 +1,13 @@
 package introspect
 
 import (
-	"encoding/json"
-	"errors"
-	"net/http"
-	"net/url"
-	"strings"
-	"time"
-
+	introspection "github.com/arsmn/oauth2-introspection"
 	"github.com/gofiber/fiber"
-)
-
-var (
-	errMalformedToken = errors.New("Missing or malformed token")
 )
 
 // Config holds the configuration for the middleware
 type Config struct {
-	// IntrospectionURL is introspection endpoint of OAuth server.
-	// Required. Default: ""
-	IntrospectionURL string
+	introspection.Config
 
 	// AuthScheme is the scheme of Authorization header.
 	// Optional. Default: "Bearer"
@@ -29,35 +17,17 @@ type Config struct {
 	// Optional. Default: "user"
 	ContextKey string
 
-	// Scopes defines required scopes for authorization.
-	// Optional. Default: nil
-	Scopes []string
-
-	// Audience defines required audience for authorization.
-	// Optional. Default: nil
-	Audience []string
-
-	// Issuers defines required issuers for authorization.
-	// Optional. Default: nil
-	Issuers []string
-
-	// ScopeStrategy is a strategy for matching scopes.
-	// Used when Scoped field has values.
-	// Optional. Default: nil
-	ScopeStrategy func([]string, string) bool
-
 	// TokenLookup is a function that is used to look up token.
 	// Optional. Default: TokenFromHeader
 	TokenLookup func(*fiber.Ctx) string
 
-	// IntrospectionRequestHeaders is list of headers
-	// that is send to introspection endpoint.
-	// Optional. Default: nil
-	IntrospectionRequestHeaders map[string]string
-
 	// Unauthorized defines the response body for unauthorized responses.
 	// Optional. Default: func(c *fiber.Ctx) string { c.SendStatus(401) }
 	Unauthorized func(*fiber.Ctx)
+
+	// Forbidden defines the response body for forbidden responses.
+	// Optional. Default: func(c *fiber.Ctx) string { c.SendStatus(403) }
+	Forbidden func(*fiber.Ctx)
 
 	// ErrorHandler is a function for handling unexpected errors.
 	// Optional. Default: func(c *fiber.Ctx, err error) string { c.SendStatus(500) }
@@ -70,26 +40,6 @@ type Config struct {
 	// Filter defines a function to skip middleware.
 	// Optional. Default: nil
 	Filter func(*fiber.Ctx) bool
-
-	client *http.Client
-}
-
-// Session represents token data and stores in context.
-type Session struct {
-	Subject string
-	Extra   map[string]interface{}
-}
-
-type introspectionResult struct {
-	Active    bool                   `json:"active"`
-	Extra     map[string]interface{} `json:"ext"`
-	Subject   string                 `json:"sub,omitempty"`
-	Username  string                 `json:"username"`
-	Audience  []string               `json:"aud"`
-	TokenType string                 `json:"token_type"`
-	Issuer    string                 `json:"iss"`
-	ClientID  string                 `json:"client_id,omitempty"`
-	Scope     string                 `json:"scope,omitempty"`
 }
 
 // New creates an introspection middleware for use in Fiber
@@ -103,32 +53,34 @@ func New(config ...Config) func(*fiber.Ctx) {
 	if cfg.ContextKey == "" {
 		cfg.ContextKey = "user"
 	}
+
 	if cfg.AuthScheme == "" {
 		cfg.AuthScheme = "Bearer"
 	}
+
 	if cfg.Unauthorized == nil {
 		cfg.Unauthorized = func(c *fiber.Ctx) {
 			c.SendStatus(fiber.StatusUnauthorized)
 		}
 	}
-	if cfg.TokenLookup == nil {
-		cfg.TokenLookup = TokenFromHeader(fiber.HeaderAuthorization, cfg.AuthScheme)
+
+	if cfg.Forbidden == nil {
+		cfg.Forbidden = func(c *fiber.Ctx) {
+			c.SendStatus(fiber.StatusForbidden)
+		}
 	}
+
 	if cfg.ErrorHandler == nil {
 		cfg.ErrorHandler = func(c *fiber.Ctx, err error) {
-			if err == errMalformedToken {
-				c.Status(fiber.StatusBadRequest)
-				c.SendString(err.Error())
-				return
-			}
 			c.SendStatus(fiber.StatusInternalServerError)
 		}
 	}
 
-	cfg.client = &http.Client{
-		Timeout: time.Millisecond * 500,
+	if cfg.TokenLookup == nil {
+		cfg.TokenLookup = TokenFromHeader(fiber.HeaderAuthorization, cfg.AuthScheme)
 	}
 
+	var introspector = introspection.New(cfg.Config)
 	return func(c *fiber.Ctx) {
 
 		if cfg.Filter != nil && cfg.Filter(c) {
@@ -137,93 +89,22 @@ func New(config ...Config) func(*fiber.Ctx) {
 		}
 
 		token := cfg.TokenLookup(c)
-		if token == "" {
-			cfg.ErrorHandler(c, errMalformedToken)
-			return
-		}
+		result, err := introspector.Introspect(token)
 
-		body := url.Values{"token": {token}}
-		if cfg.ScopeStrategy == nil {
-			body.Add("scope", strings.Join(cfg.Scopes, " "))
-		}
-
-		introspectReq, err := http.NewRequest(http.MethodPost, cfg.IntrospectionURL, strings.NewReader(body.Encode()))
 		if err != nil {
-			cfg.ErrorHandler(c, err)
-			return
-		}
-
-		for key, value := range cfg.IntrospectionRequestHeaders {
-			introspectReq.Header.Set(key, value)
-		}
-
-		introspectReq.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-		resp, err := cfg.client.Do(introspectReq)
-		if err != nil {
-			cfg.ErrorHandler(c, err)
-			return
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			cfg.Unauthorized(c)
-			return
-		}
-
-		var i introspectionResult
-		if err := json.NewDecoder(resp.Body).Decode(&i); err != nil {
-			cfg.ErrorHandler(c, err)
-			return
-		}
-
-		if len(i.TokenType) > 0 && i.TokenType != "access_token" {
-			cfg.Unauthorized(c)
-			return
-		}
-
-		if !i.Active {
-			cfg.Unauthorized(c)
-			return
-		}
-
-		for _, audience := range cfg.Audience {
-			if !contains(i.Audience, audience) {
+			switch err {
+			case introspection.ErrUnauthorized:
 				cfg.Unauthorized(c)
-				return
+			case introspection.ErrForbidden:
+				cfg.Forbidden(c)
+			default:
+				cfg.ErrorHandler(c, err)
 			}
+			return
 		}
 
-		if len(cfg.Issuers) > 0 {
-			if !contains(cfg.Issuers, i.Issuer) {
-				cfg.Unauthorized(c)
-				return
-			}
-		}
+		c.Locals(cfg.ContextKey, result)
 
-		if cfg.ScopeStrategy != nil {
-			for _, scope := range cfg.Scopes {
-				if !cfg.ScopeStrategy(strings.Split(i.Scope, " "), scope) {
-					cfg.Unauthorized(c)
-					return
-				}
-			}
-		}
-
-		if len(i.Extra) == 0 {
-			i.Extra = make(map[string]interface{})
-		}
-
-		i.Extra["username"] = i.Username
-		i.Extra["client_id"] = i.ClientID
-		i.Extra["scope"] = i.Scope
-
-		s := Session{
-			Subject: i.Subject,
-			Extra:   i.Extra,
-		}
-
-		c.Locals(cfg.ContextKey, s)
 		if cfg.SuccessHandler != nil {
 			cfg.SuccessHandler(c)
 		}
@@ -263,88 +144,4 @@ func TokenFromCookie(name string) func(*fiber.Ctx) string {
 	return func(c *fiber.Ctx) string {
 		return c.Cookies(name)
 	}
-}
-
-// Scope Strategies were copied from https://github.com/ory/fosite
-
-// HierarchicScopeStrategy ...
-func HierarchicScopeStrategy(haystack []string, needle string) bool {
-	for _, this := range haystack {
-		// foo == foo -> true
-		if this == needle {
-			return true
-		}
-
-		// picture.read > picture -> false (scope picture includes read, write, ...)
-		if len(this) > len(needle) {
-			continue
-		}
-
-		needles := strings.Split(needle, ".")
-		haystack := strings.Split(this, ".")
-		haystackLen := len(haystack) - 1
-		for k, needle := range needles {
-			if haystackLen < k {
-				return true
-			}
-
-			current := haystack[k]
-			if current != needle {
-				break
-			}
-		}
-	}
-
-	return false
-}
-
-// ExactScopeStrategy ...
-func ExactScopeStrategy(haystack []string, needle string) bool {
-	return contains(haystack, needle)
-}
-
-// WildcardScopeStrategy ...
-func WildcardScopeStrategy(matchers []string, needle string) bool {
-	needleParts := strings.Split(needle, ".")
-	for _, matcher := range matchers {
-		matcherParts := strings.Split(matcher, ".")
-
-		if len(matcherParts) > len(needleParts) {
-			continue
-		}
-
-		var noteq bool
-		for k, c := range strings.Split(matcher, ".") {
-			// this is the last item and the lengths are different
-			if k == len(matcherParts)-1 && len(matcherParts) != len(needleParts) {
-				if c != "*" {
-					noteq = true
-					break
-				}
-			}
-
-			if c == "*" && len(needleParts[k]) > 0 {
-				// pass because this satisfies the requirements
-				continue
-			} else if c != needleParts[k] {
-				noteq = true
-				break
-			}
-		}
-
-		if !noteq {
-			return true
-		}
-	}
-
-	return false
-}
-
-func contains(s []string, v string) bool {
-	for _, vv := range s {
-		if vv == v {
-			return true
-		}
-	}
-	return false
 }
